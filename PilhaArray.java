45cace739aee8be633d51302715134193a4b91c8
Ana Barbosa (Escolar) <nokisegunda@gmail.com>
2026-03-23 17:58:35 +0000

    corrige erros

--- PilhaArray.java
+++ PilhaArray.java
@@ -1,9 +1,9 @@
-public class PilhaArray implements Pilha
+public class PilhaArray implements Pilha {
     // Atributos
     private int capacidade;
     private Object[] a;
     private int t;
-    private fc;
+    private int fc;
     // Construtor
     public PilhaArray(int capacidade, int crescimento){
         this.capacidade = capacidade;
@@ -11,13 +11,13 @@ public class PilhaArray implements Pilha
         fc =  crescimento;
         if (crescimento <= 0) fc = 0;
         a = new Object[capacidade];
-
+    }
     public void push(Object o){
         if (t >= capacidade-1){
             if (fc == 0 ) capacidade*=2;
             else capacidade+=fc;
             Object b[] = new Object[capacidade];
-            for (int f = 0; f < a.legth; f++)
+            for (int f = 0; f < a.length; f++)
                 b[f] = a[f];
             a = b;
         }
@@ -33,7 +33,7 @@ public class PilhaArray implements Pilha
 
     public Object top() throws PilhaVaziaExcecao{
         if(isEmpty())
-            throw new PilhaVaziaExcecao("Pilha está vazia")
+            throw new PilhaVaziaExcecao("Pilha está vazia");
         return a[t];
     }
 
--- testePilha.java
+++ testePilha.java
@@ -13,16 +13,19 @@
  */
 public class testePilha {
 
-	public static void main(String[] args) {		
+	public static void main(String[] args) {	
+
 		Integer[] b = new Integer[1];		
-		PilhaArray pp=new PilhaArray(1,0);
+		PilhaArray pp = new PilhaArray(1,0);
+
 		System.out.println("inserindo");
-		for(int f=0;f<10;f++){
-		  System.out.println(f);		  
-		  pp.push(new Integer(f));
+		for(int f=0; f<10; f++){
+			System.out.println(f);		  
+			pp.push(new Integer(f));
 		}
+		
 		System.out.println("retirando");
-		for(int f=0;f<10;f++){
+		for(int f=0; f<10; f++){
 			  System.out.print(f);
 			  System.out.println(" - "+pp.pop());
 		}
