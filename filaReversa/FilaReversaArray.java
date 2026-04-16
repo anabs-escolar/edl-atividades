b117101c57c78376995456c9fb7d88fa47214f19
Ana Barbosa (Escolar) <nokisegunda@gmail.com>
2026-04-16 02:26:19 +0000

    adiciona nova fila

--- filaReversa/FilaReversaArray.java
+++ filaReversa/FilaReversaArray.java
@@ -1,151 +1,72 @@
 
 public class FilaReversaArray implements FilaReversa {
-    private int inicio, fim;
-    private int capacity, reverse;
+    private int inicio;
+    private int capacity, reverse, size;
     private Object[] fila;
 
 
     public FilaReversaArray(int c) {
         this.inicio = 0;
-        this.fim = 0;
         this.capacity = c;
         this.fila = new Object[c];
         this.reverse = 0;
+        this.size = 0; 
     }
 
 
     public void enqueue(Object o) {
+        if (size==capacity)
+            resize(capacity*2);
         if (reverse==0){
-            if (size() == capacity-1){
-                int n_cap = capacity*2;
-
-                Object[] n_fila = new Object[n_cap];
-                int p = inicio;
-                for (int i=0; i<size(); i++){
-                    n_fila[i] = fila[p];
-                    p = (p+1) % capacity;
-                }
-                fila = n_fila;
-                capacity = n_cap;
-                inicio = 0;
-                fim = size();
-            }
-            fila[fim] = o;
-            fim = ++fim % capacity;
-
-            if (size() == capacity/3){
-                int n_cap = capacity/2;
-                
-                Object[] n_fila = new Object[n_cap];
-                int p = inicio;
-                for (int i=0; i<size(); i++){
-                    n_fila[i] = fila[p];
-                    p = (p+1) % capacity;
-                }
-                fila = n_fila;
-                capacity = n_cap;
-                inicio = 0;
-                fim = size();
-            }
-        } else {
-            if(size() == capacity-1){
-                int n_cap = capacity*2;
-
-                Object[] n_fila = new Object[n_cap];
-                int ult = fim;
-                for (int i=0; i<size(); i++){
-                    n_fila[i] = fila[ult];
-                    ult = ++ult % capacity;
-                }
-                fila = n_fila;
-                capacity = n_cap;
-                inicio = size() -1;
-                fim = capacity;
-            }
-            
-            fila[fim] = o;
-            fim = (fim-1+capacity)%capacity;
-
-            if (size() == capacity/3){
-                int n_cap = capacity/2;
-
-                Object[] n_fila = new Object[n_cap];
-                int ult = fim;
-                for (int i=0; i<size(); i++){
-                    n_fila[i] = fila[ult];
-                    ult = (ult+1) % capacity;
-                }
-                fila = n_fila;
-                capacity = n_cap;
-                inicio = size() -1;
-                fim = capacity;
-            }
-
+            int posi = (inicio+size)%capacity;
+            fila[posi] = o;
         }
+        else {
+            inicio = (inicio+capacity-1)%capacity;
+            fila[inicio] = o;
+        }
+
+        size++;
     }
 
     public Object dequeue() throws FilaVaziaExececao {
         if(isEmpty()){
             throw new FilaVaziaExececao("A Fila está vazia.");
         }
-        Object remove = fila[inicio];
+        Object remove;
         if (reverse==0){
-            inicio = ++inicio%capacity;
-            if (size()==capacity/3){
-                int n_cap = capacity/2;
-                
-                Object[] n_fila = new Object[n_cap];
-                int p = inicio;
-                for (int i=0; i<size(); i++){
-                    n_fila[i] = fila[p];
-                    p = (p+1) % capacity;
-                }
-                fila = n_fila;
-                capacity = n_cap;
-                inicio = 0;
-                fim = size();
-
-            }
+            remove = fila[inicio];
+            inicio = (inicio+1)%capacity;
         } else {
-            inicio = (inicio+capacity-1)%capacity;
-            if (size()==capacity/3){
-                int n_cap = capacity/2;
-
-                Object[] n_fila = new Object[n_cap];
-                int ult = fim;
-                for (int i=0; i<size(); i++){
-                    n_fila[i] = fila[ult];
-                    ult = (ult+1) % capacity;
-                }
-                fila = n_fila;
-                capacity = n_cap;
-                inicio = size() -1;
-                fim = capacity;
-            }
+            int pos = (inicio+size-1)%capacity;
+            remove = fila[pos];
         }
+        size--;
+        if(capacity >1  && size <=capacity/3 )
+            resize(capacity/2);
 
         return remove;
     }
 
     public void reverse(){
-        reverse = ++reverse%2;
-        int tmp = inicio;
-        if (reverse==1){
-            inicio = (fim-1+capacity)%capacity;
-            fim = (tmp-1+capacity)%capacity;
-        } else {
-            inicio = (fim+1)%capacity;
-            fim = (tmp+1)%capacity;
+        if (reverse==0){
+            reverse++;
+        }else{
+            reverse--;
         }
     }
-
-    public int size(){ 
-        if (reverse == 0){
-            return (capacity-inicio+fim)%capacity;
+    public void resize(int n_cap){
+        Object[] n_fila = new Object[n_cap];
+        for(int i=0; i<size; i++){
+            int index = (inicio+i)%capacity;
+            n_fila[i] = fila[index];
         }
-        return (capacity+inicio-fim)%capacity;
-     }
+        fila = n_fila;
+        capacity = n_cap;
+        inicio = 0;
+    }
+    public int size(){ return size; }
 
-    public boolean isEmpty(){ return fim == inicio; }
+    public boolean isEmpty(){ return size==0; }
 
 }
